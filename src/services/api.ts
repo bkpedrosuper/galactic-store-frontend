dd076bfd14b5ea00fa1bbd53c7668f1231da6d64
Pedro Serpa <pedroserpah@gmail.com>
2021-06-14 01:12:41 -0300

    Removing console log

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,6 @@ const baseURL = process.env.NODE_ENV === 'development'
                 ? "http://localhost:3300"
                 : "https://galactic-store.herokuapp.com"
 
-console.log({baseURL, env: process.env.NODE_ENV});
-
 const api = axios.create({
     baseURL: baseURL,
 });
