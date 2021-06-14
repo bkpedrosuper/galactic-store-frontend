d134fc260b2f09cd856a0c98ba9fbaf0f7ebf241
Pedro Serpa <pedroserpah@gmail.com>
2021-06-14 01:03:39 -0300

    Test env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,11 @@
+import { LocalConvenienceStoreOutlined } from '@material-ui/icons';
 import axios from 'axios'
 
 const baseURL = process.env.NODE_ENV === 'development'
                 ? "http://localhost:3300"
                 : "https://galactic-store.herokuapp.com"
+
+console.log({baseURL, env: process.env.NODE_ENV});
 
 const api = axios.create({
     baseURL: baseURL,
